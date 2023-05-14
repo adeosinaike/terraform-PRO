731ae4e4b7d932bd6a08dc26e4ab79729dcd5531
adeosinaike <adeoluosinaike@gmail.com>
2023-05-14 01:56:11 -0400

    vpc configuration completed in main file

--- simple-vpc/main.tf
+++ simple-vpc/main.tf
@@ -1,1 +1,10 @@
+resource "aws_vpc" "norlerge_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
+  tags = {
+    Name = "norlerge_vpc"
+  }
+}
